f35567280311b0cea8c568cb76ca85061f344afa
Preet Modh <preetmodh@gmail.com>
2022-08-14 14:25:31 +0530

    body parser for  json/urlencoded

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-
+const bodyParser = require('body-parser');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+//logging request to console middleware
 app.use(morgan('dev'));
+
+//body-parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
 //routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+//error handling middleware
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+})
 
 module.exports = app;
