bae30528f3a40fd1cd8b54cba085821a7c066072
Preet Modh <preetmodh@gmail.com>
2022-08-15 14:58:41 +0530

    routes for users in app.js

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-
+//routes import 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
+
 
 mongoose.connect('mongodb+srv://node-rest-shop:'+process.env.MONGO_ATLAS_PASSWORD+'@node-rest-inventory.agjs6uh.mongodb.net/?retryWrites=true&w=majority');
 mongoose.Promise = global.Promise;
@@ -35,6 +37,7 @@ app.use((req, res, next) => {
 //routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/users', userRoutes);
 
 //error handling middleware
 app.use((req, res, next) => {
