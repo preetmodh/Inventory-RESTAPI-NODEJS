5ff9e4b57333c032149f4a6c43382b32c256f9e4
Preet Modh <preetmodh@gmail.com>
2022-08-15 16:12:40 +0530

    jwt_key cant be obtained from env

--- api/routes/user.js
+++ api/routes/user.js
@@ -69,7 +69,7 @@ router.post("/login", (req, res, next) => {
               email: user[0].email,
               userId: user[0]._id
             },
-            ""+process.env.JWT_KEY,
+            'secret_key',
             {
                 expiresIn: "1h"
             }
