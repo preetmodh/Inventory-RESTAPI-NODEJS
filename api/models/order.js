86f07cf2fd5306edfe3180d11136bb4a53462853
Preet Modh <preetmodh@gmail.com>
2022-08-15 14:36:38 +0530

    redordering models

--- api/models/order.js
+++ api/models/order.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const orderSchema = new mongoose.Schema({
     _id : mongoose.Schema.Types.ObjectId,
+    quantity: {type: Number, default: 1},
     product: {type: mongoose.Schema.Types.ObjectId, ref: 'Product'},
-    quantity: {type: Number, default: 1}
+    
 });
 
 module.exports = mongoose.model('Order',orderSchema);
